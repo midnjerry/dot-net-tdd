cd841e3f95859c5d4a4961f7d2f53b87a0c5278d
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-09-29 13:10:22 -0500

    Meagan's awesome solution for PrimeFactors

---
+++ dot-net-tdd-tests/PrimeFactorsTest.cs
@@ -0,0 +1,79 @@
+﻿using dot_net_tdd;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace dot_net_tdd_tests
+{
+    [TestClass]
+    public class PrimeFactorsTest
+    {
+        PrimeFactors factors = new PrimeFactors();
+        List<int> expected = new List<int>();
+
+        [TestMethod]
+        public void InputOfZeroAndOneReturnsEmptyList()
+        {
+            CollectionAssert.AreEqual(expected, factors.Generate(0));
+            CollectionAssert.AreEqual(expected, factors.Generate(1));
+        }
+
+        [TestMethod]
+        public void InputOfTwoReturnsTwo()
+        {
+            expected.Add(2);
+            CollectionAssert.AreEqual(expected, factors.Generate(2));
+        }
+        [TestMethod]
+        public void InputOFThreeReturns3()
+        {
+            expected.Add(3);
+            CollectionAssert.AreEqual(expected, factors.Generate(3));
+        }
+
+        [TestMethod]
+        public void InputOfFourShouldReturn2_2()
+        {
+            expected.Add(2);
+            expected.Add(2);
+            CollectionAssert.AreEqual(expected, factors.Generate(4));
+        }
+
+        [TestMethod]
+        public void InputOfFiveShouldReturn5()
+        {
+            expected.Add(5);
+            CollectionAssert.AreEqual(expected, factors.Generate(5));
+        }
+
+        [TestMethod]
+        public void InputOfTwelveShouldReturn2_2_3()
+        {
+            expected.Add(2);
+            expected.Add(2);
+            expected.Add(3);
+            CollectionAssert.AreEqual(expected, factors.Generate(12));
+        }
+
+        [TestMethod]
+        public void InputOf541()
+        {
+            expected.Add(541);
+            CollectionAssert.AreEqual(expected, factors.Generate(541));
+        }
+
+        [TestMethod]
+        public void InputOf1299709()
+        {
+            expected.Add(1299709);
+            CollectionAssert.AreEqual(expected, factors.Generate(1299709));
+        }
+
+        [TestMethod]
+        public void InputOf703142569()
+        {
+            expected.Add(541);
+            expected.Add(  1299709);
+            CollectionAssert.AreEqual(expected, factors.Generate(703142569));
+        }
+    }
+}
---
+++ dot-net-tdd/PrimeFactors.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace dot_net_tdd
+{
+    public class PrimeFactors
+{
+        public List<int> Generate(int num)
+        {
+            List<int> result = new List<int>();
+
+            int temp = num;
+
+            for (int counter = 2; counter <= temp; counter++)
+            {
+                while (temp % counter == 0)
+                {
+                    temp /= counter;
+                    result.Add(counter);
+                }
+            }
+           
+                  
+            return result;
+        }
+}
+}
